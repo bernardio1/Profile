dd40d83ce3c7f845187e37f608c901af387c8454
bernardio1 <109044709+bernardio1@users.noreply.github.com>
2022-08-09 17:24:19 -0400

    Delete good.js

--- good.js
+++
@@ -1,6 +0,0 @@
-document.write(
-  "I'm current graduate student at the University of Pennsylvania studying computer science with focus on Artificial intelligence.
-i believe it's important for people to have role models that look more like them in positions that they aspire to be.
-i hope to show aspiring engineers that despite the current demographic in stem  fields, they should should not be deterred in their ambitions
-my research interests involve automony and the human-robot interaction."
-)
